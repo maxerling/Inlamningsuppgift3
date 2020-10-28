a03a7617175afbc98bd8f7a26fe2247e3d31a315
maxerling <maximilianerling@gmail.com>
2020-10-28 20:32:16 +0100

    added instance variables

---
+++ src/Puzzle.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+import java.util.Random;
+
+/**
+ * Created by Max Erling
+ * Date: 2020-10-23
+ * Copyright: MIT
+ * Class: Java20B
+ */
+
+public class Puzzle {
+    private Random random = new Random();
+    private JButton[][] buttons;
+    private JButton newGame = new JButton("Nytt spel");
+    private int size;
+
+}
